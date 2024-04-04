97a2fd979b72a4274bfdf6bea5d8938050f7eafb
ali-zargari <77089778+ali-zargari@users.noreply.github.com>
2024-04-04 15:37:12 -0700

    server.js is connected to the database. Can make queries.

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,45 @@
 const express = require('express');
+const mysql = require('mysql2/promise'); // import mysql2
+
+
 const app = express();
 const port = process.env.PORT || 3000; // It's important to use process.env.PORT for deployment.
 
+
+const pool = mysql.createPool({
+    host: 'mysql-206af299-sjsu-b628.a.aivencloud.com',
+    user: 'avnadmin',
+    // Specify the database (schema) name here
+    database: 'CS_157A_Project',
+    password: 'AVNS_KPqKJ44iZGhPb5xCUgA',
+    port: 19243,
+    ssl: {
+        rejectUnauthorized: false // Set to false only if necessary and you understand the implications
+    }
+});
+
+
+
 app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.json());
+
+app.post('/test', async (req, res) => { // You can now use async function
+    console.log(req.body);
+
+
+
+    try {
+        const connection = await pool.getConnection();
+        const [rows] = await connection.execute(
+            'SELECT * FROM Users'
+        );
+        connection.release();
+
+        res.send(rows);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
-
-
-
-
-
