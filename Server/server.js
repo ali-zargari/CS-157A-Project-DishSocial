ddc02f02e2f41b958672686cd249139034fb6ac3
JimNoodl8 <99715807+JimNoodl8@users.noreply.github.com>
2024-04-26 14:25:30 -0700

    Revert "Update server.js"
    This reverts commit f8757b44622bb8475af023b614c230aedb8fdbcd.

--- Server/server.js
+++ Server/server.js
@@ -87,25 +87,20 @@ app.get('/users/friends', async (req, res) => {
                 [userID]
         );
 
-        //console.log(friendResult);
-
         // Extracting user IDs from the friendResult
         const friendIds = friendResult.map(friend => friend.UserID2);
 
-        //console.log(friendIds);
         // Check if there are friend IDs to query
         if (friendIds.length > 0) {
             // Query to get names of friends
-            const friendIdsPlaceHolders = friendIds.map(() => '?').join(',');
-
             const [rows] = await connection.execute(
-               `SELECT FirstName, LastName FROM Users WHERE UserID IN (${friendIdsPlaceHolders})`,
-                friendIds
+                'SELECT FirstName, LastName FROM Users WHERE UserID IN (?)',
+                [`(${friendIds.join(',')})`]
             );
 
-            //console.log(rows)
+            console.log(rows)
             res.send(rows);
-            //console.log(`(${friendIds.join(',')})`);
+            console.log(`(${friendIds.join(',')})`);
         } else {
             res.send([]); // No friends found
         }
